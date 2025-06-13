eeb6e44ed230ff01116622ad3a511e0ae163bb16
NotMohamedKhaled <mohamedkhaled191817@gmail.com>
2025-06-13 16:39:25 +0300

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderCartItems } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js'
+import '../data/backend-practice.js'
 renderCheckoutHeader();
 renderCartItems();
 renderPaymentSummary();
